5606771aa2ddcd2bdde7db28e4d579e835bde1aa
woongsnote <woongsnote@gmail.com>
2023-05-23 16:35:40 +0900

    refactor: useDebounce hook arrow function apply

--- src/hooks/useDebounce.ts
+++ src/hooks/useDebounce.ts
@@ -1,25 +1,25 @@
 import { useState, useEffect, useRef } from 'react';
 import { debounce } from '../utils';
 
-export function useDebounce<T>(value: T, delay: number): T {
-  const [debouncedValue, setDebouncedValue] = useState<T>(value);
-  const timeoutIdRef = useRef<NodeJS.Timeout | undefined>(undefined);
+export const useDebounce = <T>(value: T, delay: number): T => {
+    const [debouncedValue, setDebouncedValue] = useState<T>(value);
+    const timeoutIdRef = useRef<NodeJS.Timeout | undefined>(undefined);
 
-  useEffect(() => {
-    const debouncedFn = debounce(() => {
-      setDebouncedValue(value);
-    }, delay);
+    useEffect(() => {
+        const debouncedFn = debounce(() => {
+            setDebouncedValue(value);
+        }, delay);
 
-    if (timeoutIdRef.current) {
-      clearTimeout(timeoutIdRef.current);
-    }
+        if (timeoutIdRef.current) {
+            clearTimeout(timeoutIdRef.current);
+        }
 
-    timeoutIdRef.current = setTimeout(debouncedFn, delay);
+        timeoutIdRef.current = setTimeout(debouncedFn, delay);
 
-    return () => {
-      clearTimeout(timeoutIdRef.current);
-    };
-  }, [value, delay]);
+        return () => {
+            clearTimeout(timeoutIdRef.current);
+        };
+    }, [value, delay]);
 
-  return debouncedValue;
+    return debouncedValue;
 }
