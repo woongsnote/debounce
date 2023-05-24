fb56a4e6d8f26e46dcbf3c233f7836a249fd246c
woongsnote <woongsnote@gmail.com>
2023-05-24 13:32:33 +0900

    feat: loading state spinner add

--- src/hooks/useDebouncedInput.ts
+++ src/hooks/useDebouncedInput.ts
@@ -1,24 +1,31 @@
-import { ChangeEvent, useState } from "react"
+import { ChangeEvent, useState } from "react";
 import { useDebounce } from "./useDebounce";
 
 export const useDebouncedInput = (delay: number) => {
-    const [inputValue, setInputValue] = useState<string>('');
-    const [debouncedValue, setDebouncedValue] = useState<string>('');
+  const [inputValue, setInputValue] = useState<string>("");
+  const [debouncedValue, setDebouncedValue] = useState<string>("");
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
-    const debouncedSearch = useDebounce((value: string) => {
-        console.log('Search for', value);
-        setDebouncedValue(value);
+  const debouncedSearch = useDebounce((value: string) => {
+    setIsLoading(true);
+
+    setTimeout(() => {
+      console.log("Search for", value);
+      setDebouncedValue(value);
+      setIsLoading(false);
     }, delay);
+  }, delay);
 
-    const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-        const { value } = event.target;
-        setInputValue(value);
-        debouncedSearch(value);
-    }
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    setInputValue(value);
+    debouncedSearch(value);
+  };
 
-    return {
-        inputValue, handleChange, debouncedValue
-    }
-
-}
-
+  return {
+    inputValue,
+    handleChange,
+    debouncedValue,
+    isLoading,
+  };
+};
