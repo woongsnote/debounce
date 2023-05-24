15894904d53ca60d6398b707f6bb393b42291f5c
woongsnote <woongsnote@gmail.com>
2023-05-24 13:30:31 +0900

    refactor: unnecessary files remove

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import "./App.css";
 import { useDebouncedInput } from "./hooks/useDebouncedInput";
 import { DELAY_IN_MS } from "./constants";
 import DebouncedSearchInput from "./components/DebouncedSearchInput";
@@ -6,12 +5,16 @@ import Container from "./components/Container";
 import DebouncedResult from "./components/DebouncedResult";
 
 function App() {
-  const { inputValue, debouncedValue, handleChange } =
+  const { inputValue, debouncedValue, handleChange, isLoading } =
     useDebouncedInput(DELAY_IN_MS);
 
   return (
     <Container>
-      <DebouncedSearchInput inputValue={inputValue} handleChange={handleChange} />
+      <DebouncedSearchInput
+        inputValue={inputValue}
+        handleChange={handleChange}
+        isLoading={isLoading}
+      />
       <DebouncedResult result={debouncedValue} />
     </Container>
   );
